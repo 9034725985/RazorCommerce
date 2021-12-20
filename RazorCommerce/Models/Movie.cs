e2d302d7dea7a8c144a9d9c79367e9591c88fcf0
kushal <kushaldeveloper@gmail.com>
2021-12-20 16:37:47 -0700

    update route

--- RazorCommerce/Models/Movie.cs
+++ RazorCommerce/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorCommerce.Models
 {
@@ -6,10 +7,13 @@ namespace RazorCommerce.Models
     {
         public int ID { get; set; }
         public string Title { get; set; } = string.Empty;
-
+        
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; } = string.Empty;
+        
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
